ce27df7a57a45db7a8dbcb801f01b5c558ffc5bc
thivernale <thivernale@gmail.com>
2025-03-18 18:52:20 +0200

    Update tsconfig to exclude TypeScript file extensions in includes

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 export default tseslint
   .config(
