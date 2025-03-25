e4118bf35b22295e2f086b27854a22d7cccbb0b8
thivernale <thivernale@gmail.com>
2025-03-25 13:46:57 +0200

    Enable react eslint plugin according to README.md
    Signed-off-by: thivernale <thivernale@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
 export default tseslint
   .config(
@@ -14,13 +15,21 @@ export default tseslint
       languageOptions: {
         ecmaVersion: 2020,
         globals: globals.browser,
+        parserOptions: {
+          project: ['./tsconfig.app.json', './tsconfig.node.json'],
+          tsconfigRootDir: import.meta.dirName,
+        },
       },
+      settings: { react: { version: '19.0' } },
       plugins: {
+        react,
         'react-hooks': reactHooks,
         'react-refresh': reactRefresh,
       },
       rules: {
         ...reactHooks.configs.recommended.rules,
+        ...react.configs.recommended.rules,
+        ...react.configs['jsx-runtime'].rules,
         'react-refresh/only-export-components': [
           'warn',
           { allowConstantExport: true },
