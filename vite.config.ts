05f4675e31d8e267134b286583b44f3bdb554572
thivernale <thivernale@gmail.com>
2025-03-27 16:12:49 +0200

    Dependencies and setup
    Signed-off-by: thivernale <thivernale@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 /// <reference types="vitest/config" />
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     globals: true,
     environment: 'jsdom',
