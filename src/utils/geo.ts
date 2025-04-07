e526e8336f390a698b37eb53d81e12d092579370
thivernale <thivernale@gmail.com>
2025-04-07 23:33:01 +0300

    Change geo api
    Signed-off-by: thivernale <thivernale@gmail.com>

--- src/utils/geo.ts
+++ src/utils/geo.ts
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
-const GEO_API_URL = 'http://ip-api.com/json/?fields=countryCode';
+// const GEO_API_URL = 'http://ip-api.com/json/?fields=countryCode';
+const GEO_API_URL = `https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.VITE_GEO_API_KEY}`;
+
+// type ResponseType = { countryCode?: string };
+type ResponseType = { location: { country: string } };
 
 export async function getCountryCode() {
   try {
-    const res = await axios.get<{ countryCode?: string }>(`${GEO_API_URL}`);
-    return res.data.countryCode as string;
+    const { data } = await axios.get<ResponseType>(`${GEO_API_URL}`);
+    return data.location.country as string;
   } catch (err: unknown) {
     console.log(err);
     return '';
